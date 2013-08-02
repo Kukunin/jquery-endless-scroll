0392fdf43a95dcca8a3a238a7a1a95936942d0dd
Sergey Kukunin <sergey.kukunin@gmail.com>
2013-08-02 18:30:54 +0300

    Add jquery plugin skeleton

---
+++ src/jquery.endless-scroll.js
@@ -0,0 +1,15 @@
+/**
+ * Author: Sergey Kukunin
+ * See: https://github.com/Kukunin/jquery-endless-scroll
+ */
+(function($) {
+
+	$.endlessScroll = function(options) {
+		options = $.extend({
+
+		}, options);
+
+		return this;
+	}
+
+})(jQuery);
