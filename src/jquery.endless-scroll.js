d0a211e1f68e0076c35348ca0a8724ed818377c1
Sergey Kukunin <sergey.kukunin@gmail.com>
2013-08-05 11:24:59 +0300

    Add support of modules

--- src/jquery.endless-scroll.js
+++ src/jquery.endless-scroll.js
@@ -4,10 +4,31 @@
  */
 (function($) {
 
-	$.endlessScroll = function(options) {
-		options = $.extend({
+	//Declaration of modules
+	var scrollModule = {
+		init: function(options, obj) {
+			obj.options = $.extend({
+			}, options);
+		}
+	}
 
+	$.fn.endlessScroll = function(options) {
+
+		//Initialize modules
+		this.options = $.extend(true, {
+			scrollContainer: window,
+			_modules: [ scrollModule ],
+			modules: []
 		}, options);
+
+
+		//Merge custom options with default
+		$.merge(this.options.modules, this.options._modules);
+		//Init modules
+		this.options.modules.forEach($.proxy(function(module) {
+			module.init(this.options, this);
+		},this));
+
 
 		return this;
 	}
