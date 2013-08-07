61c0801288c5b4ae835ca70f0a1338c12696b09f
Sergey Kukunin <sergey.kukunin@gmail.com>
2013-08-07 11:48:45 +0300

    Add 4th argument for jes:afterPageLoad
    Pass the new entities list as 4th argument

--- src/jquery.endless-scroll.js
+++ src/jquery.endless-scroll.js
@@ -168,7 +168,7 @@
 				if ( $.isFunction(callback) ) {
 					callback(data);
 				}
-				this.container.trigger("jes:afterPageLoad", [url, placement, data]);
+				this.container.trigger("jes:afterPageLoad", [url, placement, data, entities]);
 			}, this), 'html');
 		},
 		setMarker: function(entity, url) {
